05d09976b664c17f38551d841ba2c730823df7a7
BJones <byroncjones19@gmail.com>
2026-03-24 20:05:40 -0400

    Update
    Added a new materal for the Guard AI controller.

--- Assets/Scripts/GuardAI.cs
+++ Assets/Scripts/GuardAI.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class GuardController : MonoBehaviour
+public class GuardAI : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
