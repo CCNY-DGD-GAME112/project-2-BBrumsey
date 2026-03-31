4fd379a36be1db1525a689a00ba51df5acf5a5b2
BJones <byroncjones19@gmail.com>
2026-03-31 15:18:23 -0400

    Update
    Add a Focal Point to the Main camera to Follow the player as he travels in game.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,33 @@
+using Unity.Hierarchy;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float verticalInput;
+    private float speed = 5;
+    private float turnSpeed = 10;
+   
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        //Makes it so the player moves forward or backward:
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // Makes it so the player rotates to the right:
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    
     }
+   
+   
 }
