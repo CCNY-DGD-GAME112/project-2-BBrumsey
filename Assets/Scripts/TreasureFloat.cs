3ed6d5fc969a0a0c828ca44177d4d48e0ad93d10
BJones <byroncjones19@gmail.com>
2026-04-17 23:16:53 -0400

    Update
    Added a  particles for sparkly effect for treasure as well as make it levitate.

---
+++ Assets/Scripts/TreasureFloat.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class TreasureFloat : MonoBehaviour
+{
+    public float floatHeight = 0.25f;
+    public float floatSpeed = 2;
+    public float rotateSpeed = 60;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        float newY = startPos.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+        transform.position = new Vector3(startPos.x, newY, startPos.z);
+
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+    }
+}
